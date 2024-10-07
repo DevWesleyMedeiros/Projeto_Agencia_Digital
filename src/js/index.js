17b9d0e1d27ea42435101320f411d438d4c0b193
Wesley <DevWesleyMedeiros@yahoo.com>
2024-10-07 17:20:03 -0300

    Header em desenvolvimento

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,17 @@
-import '../styles/style.scss';
-import '@fortawesome/fontawesome-free/js/all.js';
-import * as bootstrap from 'bootstrap';
-import $ from 'jquery';
-import myImage from '../assets/images/img1.jpg';
-
+/*
+import myImage from '../assets/images/img1.jpg'; Imagens e outros arquivos devem ser importados no javascript
+Exemplo de código que usa import das imagens para usar no projeto
 const img = document.createElement('img');
 img.src = myImage;
 document.body.appendChild(img);
+*/ 
+
+import '../styles/style.scss';
+import '@fortawesome/fontawesome-free/js/all.js';
+import * as bootstrap from 'bootstrap';
+
+
+
 
 $(document).ready(function () {
     alert("Olá, mundo");
