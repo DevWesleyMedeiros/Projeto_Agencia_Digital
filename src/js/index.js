b8c02c30ff5d3e9915642a4f9a0bd13446d0dc0a
Wesley <DevWesleyMedeiros@yahoo.com>
2024-10-10 12:03:18 -0300

    Responsivo ajustado e finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,35 @@
 import '../styles/style.scss';
 import '@fortawesome/fontawesome-free/js/all.js';
-import * as bootstrap from 'bootstrap';
 import $ from 'jquery';
-
 import imageMockup1 from '../assets/images/mockup1.jpg';
 let imgTag = document.querySelector(".mockup1");
-imgTag.src = `${imageMockup1}`;
-imgTag.alt = "Imagem mini-banner CTA";
-imgTag.style.maxWidth = "500px";
-
+if (imgTag) {
+    imgTag.src = `${imageMockup1}`;
+    imgTag.alt = "Imagem mini-banner CTA";
+    imgTag.style.maxWidth = "500px";
+}
 const importImages = require.context('../assets/images', false, /\.jpg$/);
 const images = [];
-
 for (let i = 1; i <= 6; i++) {
-  images.push(importImages(`./img${i}.jpg`));
+    images.push(importImages(`./img${i}.jpg`));
 }
-const imgsTag = [...document.querySelectorAll(".IMGtag")];
-
+const imgsTag = Array.from(document.querySelectorAll(".IMGtag"));
 imgsTag.forEach((Img, index) => {
-  Img.src = images[index]; 
-  Img.style.width = "100%"; 
+    Img.src = images[index];
+    Img.style.width = "100%";
 });
-
 import Bianchini from '../assets/images/bianchini.png';
 import Rancho from '../assets/images/rancho.png';
 import Hoepcke from '../assets/images/hoepcke.png';
 import Monguillhot from '../assets/images/monguilhott.png';
-
 const arrayLogoParceiros = [Bianchini, Rancho, Hoepcke, Monguillhot];
-const logosEmpresasParceiras = [...document.querySelectorAll(".logo-empresas-clientes")];
-
+const logosEmpresasParceiras = Array.from(document.querySelectorAll(".logo-empresas-clientes"));
 for (let i = 0; i < arrayLogoParceiros.length; i++) {
     logosEmpresasParceiras[i].src = arrayLogoParceiros[i];
 }
-
 document.addEventListener("DOMContentLoaded", () => {
-    $('.Humburguer_icon').click(function(e) {
+    $('.Humburguer_icon').on('click', function (e) {
         e.stopPropagation();
-        $('.nav-mobile').parent().find('ul').toggle(1000); 
+        $('.nav-mobile').parent().find('ul').toggle(1000);
     });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: './src/js/index.js',
+    entry: './src/js/index.ts',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
